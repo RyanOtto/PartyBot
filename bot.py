5767f39061bb20332449708fc2819e6329baaa9b
Ryan Otto <rotto@drew.edu>
2018-01-15 10:19:25 -0500

    Set Bot Token

--- bot.py
+++ bot.py
@@ -408,4 +408,5 @@ async def guess(guess):
 
     else: await client.say("Start a game of Hangman with $hangman before trying to guess a letter!")
 
-client.run('Mzc0NjIwODM1MzYyNzY2ODY4.DTbSWA.WekEysBCXWRWDb4bA663tEgkcmE')
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+client.run(os.environ['DISCORD'])
